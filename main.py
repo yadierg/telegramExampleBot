5b9010f699832c62b5f4d4554727db95ad0793f9
Yadierg <yadier.desarolloweb@gmail.com>
2024-07-04 18:16:18 -0400

    Wiki Example

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import logging
 from telegram import Update
 from telegram.ext import (
-    Application,
+    ApplicationBuilder,
     CommandHandler,
     ContextTypes
 )
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#Configure logs
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-    await update.message.reply_text( "Hola Mundo" )
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Soy un bot, hablame por favor'
+    )
 
 
 def main() -> None:
@@ -17,21 +27,35 @@ def main() -> None:
     # Bot's Data
     token = '1908900556:AAHiEcK2T6K0IuK04rvpjKOg3Nj6eUOAstc'
     bot = 'yayoPrueba_bot @Yayo1088Bot'
+    weather_token = '19fdb2e28bf14c20b71232813240207'
 
     #Create Application
-    application = (
+    
+    """ application = (
         Application.builder()
         .token(token)
         .read_timeout(30)
         .write_timeout(30)
         .build()
     )
-
-    application.add_handler(CommandHandler("start", start))
+ """
+    #application.add_handler(CommandHandler("start", start))
 
     # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+    #application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == "__main__":
-    main()
+    # Bot's Data
+    token = '1908900556:AAHiEcK2T6K0IuK04rvpjKOg3Nj6eUOAstc'
+    bot = 'yayoPrueba_bot @Yayo1088Bot'
+
+    # Create App
+    application = ApplicationBuilder().token(token).build()
+
+    # Create Command
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    #Run App
+    application.run_polling()
